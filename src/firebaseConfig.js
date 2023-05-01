585557db9a944e947680072dbb4e117e50388e7b
Madan Neupane <nmadan692@gmail.com>
2023-05-01 14:54:39 +0900

    updated env variables

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,13 +1,13 @@
 // firebaseConfig.js
 
 export default {
-  apiKey: "AIzaSyDjJam0tzUBbq1I1br9eY8_t4P9uiYvw-I",
-  authDomain: "studymate-d6cf0.firebaseapp.com",
-  projectId: "studymate-d6cf0",
-  storageBucket: "studymate-d6cf0.appspot.com",
-  databaseURL: "https://studymate-d6cf0-default-rtdb.asia-southeast1.firebasedatabase.app/",
-  messagingSenderId: "720521203208",
-  appId: "1:720521203208:web:29e2d66b1ff56151bf0bbd",
-  measurementId: "G-DG38ZEV9RR"
+  apiKey: import.meta.env.API_KEY,
+  authDomain: import.meta.env.AUTH_DOMAIN,
+  projectId: import.meta.env.PROJECT_ID,
+  storageBucket: import.meta.env.STORAGE_BUCKET,
+  databaseURL: import.meta.env.DATABASE_URL,
+  messagingSenderId:import.meta.env.MESSAGE_SENDER_ID,
+  appId: import.meta.env.APP_ID,
+  measurementId: import.meta.env.MEASUREMENT_ID
 };
   
